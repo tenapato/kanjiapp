0bd6b459fe62f72cdf99b75be6a54b248c9feac8
pato tena <tenapatricio@gmail.com>
2021-04-17 21:24:21 -0500

    Small changes and directories

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import NavBar from './NavBar/NavBar';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<NavBar />, document.getElementById('root'));
